cf13c18a412b0ec9f18029f47adbaec674e1c085
Jan Martinek <jmartinek11@gmail.com>
2024-07-31 01:09:19 +0200

    feat: improved neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,7 +8,31 @@ return {
 		"3rd/image.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			-- behavior and apppearance
+			close_if_last_window = true,
+			popup_border_style = "rounded",
+            window = {
+                position = "float",
+            },
+
+            -- defines settings for sources
+			sources = { "filesystem", "document_symbols" },
+			filesystem = {
+				follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = false,
+                },
+
+			},
+            document_symbols = {
+                follow_cursor = true,
+            },
+		})
+
+        -- keybinds
 		vim.keymap.set("n", "<C-s>", ":Neotree close<CR>")
-		vim.keymap.set("n", "<C-n>", ":Neotree reveal left<CR>")
+		vim.keymap.set("n", "<C-n>", ":Neotree source=filesystem reveal=true position=right<CR>")
+		vim.keymap.set("n", "<leader>fs", ":Neotree document_symbols position=float<CR>")
 	end,
 }
