87457ad839603cf8f36fc2e13b9602ed42e47cd7
Jan Martinek <jmartinek11@gmail.com>
2024-07-31 01:08:23 +0200

    feat: gitsigns made more responsive

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,10 +1,18 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup()
+		require("gitsigns").setup({
+            current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 200,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
+		})
 
-        -- keybinds
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+		vim.keymap.set("n", "<leader>gsp", ":Gitsigns preview_hunk<CR>", {})
+		vim.keymap.set("n", "<leader>gsb", ":Gitsigns toggle_current_line_blame<CR>", {})
 	end,
 }
