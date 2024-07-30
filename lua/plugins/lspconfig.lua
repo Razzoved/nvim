d68269cbc4815e7e1f6398ebb09bc8d04fccea2a
Jan Martinek <jmartinek11@gmail.com>
2024-07-31 01:09:10 +0200

    feat: changed out some lsps/formatters/diagnostics

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,18 +1,54 @@
 return {
 	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			-- settings
+			vim.diagnostic.config({ update_in_insert = true })
+
+			-- diagnostic
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.knymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>td", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				end,
+			})
+		end,
+	},
+	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
 			"williamboman/mason.nvim",
 			"neovim/nvim-lspconfig",
 		},
+		lazy = false,
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lsp = require("cmp_nvim_lsp")
+			local capabilities = lsp.default_capabilities()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					"lua_ls",
 					"csharp_ls",
-					"intelephense",
-                    "powershell_es",
+					"dockerls",
+					"fsautocomplete", -- f#
+					"intelephense", -- php
+					"lua_ls",
+					"powershell_es",
+					"taplo", -- toml
 				},
 				handlers = {
 					function(server_name)
@@ -31,39 +67,6 @@ return {
 						})
 					end,
 				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			--local lspconfig = require("lspconfig")
-
-			-- settings
-			vim.diagnostic.config({ update_in_insert = true })
-
-			-- diagnostic
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<leader>td", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				end,
 			})
 		end,
 	},
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,19 +6,42 @@ return {
 		"nvimtools/none-ls.nvim",
 	},
 	config = function()
-		require("mason-null-ls").setup({
-			ensure_installed = {
-				-- lua
-                "stylua",
-                -- c#
-                "csharpier"
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- diagnostics
+				null_ls.builtins.diagnostics.editorconfig_checker, -- .editorconfig
+				null_ls.builtins.diagnostics.gdlint, -- godot
+				null_ls.builtins.diagnostics.hadolint, -- dockerfile
+				null_ls.builtins.diagnostics.phpcs, -- php
+				null_ls.builtins.diagnostics.selene, -- lua
+				-- formatting
+				null_ls.builtins.formatting.csharpier, -- cs
+				null_ls.builtins.formatting.fantomas, -- fsharp
+				null_ls.builtins.formatting.gdformat, -- godot
+				null_ls.builtins.formatting.pretty_php, -- php
+				null_ls.builtins.formatting.prettierd, -- npm
+				null_ls.builtins.formatting.stylua, -- lua
 			},
-			automatic_installation = false,
-			handlers = {},
 		})
-		require("null-ls").setup()
+		require("mason-null-ls").setup({
+			methods = {
+				diagnostics = true,
+				formatting = true,
+				code_actions = false,
+				completion = false,
+				hover = false,
+			},
+			automatic_installation = true,
+		})
 
 		-- keybinds
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set(
+			"n",
+			"<leader>gf",
+			"<Cmd>lua vim.lsp.buf.format({ timeout_ms = 2000 })<CR>",
+			{ noremap = true, silent = true }
+		)
+		--vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, {})
 	end,
 }
